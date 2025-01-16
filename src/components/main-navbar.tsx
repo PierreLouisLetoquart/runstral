0791b17981366ecb2d07b6a80fcd7237a0965961
Pierre-Louis Létoquart <pierrelouislet@gmail.com>
2025-01-16 10:52:09 -0500

    feat: runstral logo with dark theme

--- src/components/main-navbar.tsx
+++ src/components/main-navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { ModeToggle } from "./mode-toggle";
 import { SignoutButon } from "./signout-button";
+import Image from "next/image";
 
 // eslint-disable-next-line -- Weird eslint error saying equals to supertype (makes sense btw, but let me do my thing)
 interface NavbarProps extends React.HTMLAttributes<HTMLElement> {}
@@ -13,8 +14,26 @@ export function Navbar({ className, ...props }: NavbarProps) {
       className={cn("w-full h-16 flex items-center justify-between", className)}
       {...props}
     >
-      <Link href={"/"}>
-        <h1 className="text-xl font-bold tracking-tight">Runstral</h1>
+      <Link className="flex items-center gap-2" href={"/"}>
+        <Image
+          className="block dark:hidden"
+          src="/logo.png"
+          alt="Runstral Logo"
+          width={30}
+          height={30}
+        />
+        <Image
+          className="hidden dark:block"
+          src="/logo-dark.png"
+          alt="Runstral Logo"
+          width={30}
+          height={30}
+        />
+        <h1 className="text-lg leading-5 font-bold tracking-tight">
+          Runstral
+          <br />
+          run_
+        </h1>
       </Link>
       <div className="flex items-center gap-3">
         <ModeToggle />
