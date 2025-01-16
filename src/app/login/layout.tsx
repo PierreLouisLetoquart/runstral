bc514bfdb55dcba02b55e7665aefc9fa75651583
Pierre-Louis Létoquart <pierrelouislet@gmail.com>
2025-01-16 00:23:01 -0500

    hot fix

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,25 +1,4 @@
-export default function LoginLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <div className="w-full flex flex-col gap-6">
-      <WelcomeMessageCard />
-      <div className="w-full max-w-lg mx-auto flex flex-col gap-6">
-        <div className="flex flex-col gap-3 mt-12">
-          <h2 className="text-2xl font-bold tracking-tight">Login</h2>
-          <p className="text-muted-foreground font-light">
-            Hey you, you need to login to access the app 😳
-          </p>
-        </div>
-        {children}
-      </div>
-    </div>
-  );
-}
-
-export function WelcomeMessageCard() {
+function Hey() {
   return (
     <div className="w-full text-[#582D1D] dark:text-[#FFE0C2] space-y-3 p-[12px] rounded-[18px] bg-[#FFEFD6] border border-[#FFC182] dark:bg-[#331E0B] dark:border-[#66350C]">
       <p className="leading-7">
@@ -34,6 +13,27 @@ export function WelcomeMessageCard() {
         like this one (wait... not sure...). It is only for the demo, sorry
         😄...
       </p>
+    </div>
+  );
+}
+
+export default function LoginLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="w-full flex flex-col gap-6">
+      <Hey />
+      <div className="w-full max-w-lg mx-auto flex flex-col gap-6">
+        <div className="flex flex-col gap-3 mt-12">
+          <h2 className="text-2xl font-bold tracking-tight">Login</h2>
+          <p className="text-muted-foreground font-light">
+            Hey you, you need to login to access the app 😳
+          </p>
+        </div>
+        {children}
+      </div>
     </div>
   );
 }
